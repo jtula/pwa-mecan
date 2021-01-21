8bbcce4c779c3683c3e5f12eb6a78abfaeda5209
Jose A Tula Leyva <joseph.tula@gmail.com>
2021-01-20 22:36:06 -0500

    feat: configure tailwindcss

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Suspense } from "react";
+import MainLayout from "src/layouts/MainLayout";
+
+const Home = React.lazy(() => import("src/pages/Home"));
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Suspense fallback={null}>
+        <MainLayout>
+          <Home />
+        </MainLayout>
+      </Suspense>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,35 @@
 module.exports = {
-  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#D32F2F",
+        "primary-light": "#E51809",
+        "primary-dark": "#9E3333",
+        secondary: "#539FEC",
+        terciary: "#E2B02F",
+        gray: "#C7C4C4",
+        "gray-light": "F2EEEE",
+        "gray-dark": "959494",
+      },
+      fontFamily: {
+        roboto: ["Roboto", "sans-serif"],
+        helvetica: ["Helvetica", "sans-serif"],
+      },
+      fontSize: {
+        smaller: "12px",
+        small: "14px",
+        normal: "16px",
+        big: "20px",
+      },
+      borderRadius: {
+        normal: "2px",
+      },
+      boxShadow: {
+        normal: "0 2px 2px #80000000",
+      },
+    },
   },
   variants: {
     extend: {},
