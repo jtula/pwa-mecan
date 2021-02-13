c05a284731821da4fc8cee911681b2dc79f4804c
Jose A Tula Leyva <joseph.tula@gmail.com>
2021-02-12 21:17:26 -0500

    fix: add usercontext

---
+++ src/context/UserContext.js
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+
+const Context = React.createContext({});
+
+export function UserContextProvider({ children }) {
+  const [user, setUser] = useState(() =>
+    JSON.parse(window.sessionStorage.getItem("username"))
+  );
+
+  return (
+    <Context.Provider
+      value={{
+        user,
+        setUser,
+      }}
+    >
+      {children}
+    </Context.Provider>
+  );
+}
+
+export default Context;
