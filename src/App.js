9800e8ec03361c3f26764f24135f16d257bb5ff9
Jose A Tula Leyva <joseph.tula@gmail.com>
2021-02-22 23:23:42 -0500

    feat: create object store

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import indexedDb from "src/utils/indexdb";
 import MainLayout from "src/layouts/MainLayout";
 import Home from "src/pages/Home";
+import {
+  OBJECT_STORE_USERS,
+  OBJECT_STORE_INCOMES,
+  OBJECT_STORE_EXPENSES,
+  OBJECT_STORE_PRINTERS,
+} from "src/constants";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const runIndexDb = async () => {
+      await indexedDb.createObjectStore([
+        OBJECT_STORE_USERS,
+        OBJECT_STORE_INCOMES,
+        OBJECT_STORE_EXPENSES,
+        OBJECT_STORE_PRINTERS,
+      ]);
+      setLoading(false);
+    };
+    runIndexDb();
+  }, []);
+
+  if (loading) return null;
+
   return (
     <div className="App">
       <MainLayout>
