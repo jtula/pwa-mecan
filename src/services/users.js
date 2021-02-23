0abe1bf949d8a81b651d7a138197ac33c81c3f06
Jose A Tula Leyva <joseph.tula@gmail.com>
2021-02-22 23:24:07 -0500

    fix: register user

--- src/services/users.js
+++ src/services/users.js
@@ -1,19 +1,24 @@
 import axios from "src/utils/axios";
 import { set, get } from "idb-keyval";
+import indexedDb from "src/utils/indexdb";
+import { OBJECT_STORE_USERS } from "src/constants";
 
 export const login = (params) => {
   return axios.post("/auth/login", params).then((response) => response.data);
 };
 
 export const register = async (username) => {
-  const users = (await get("users")) || [];
+  const users = await indexedDb.getAllValue(OBJECT_STORE_USERS);
   const usernameExist = users.find((user) => user.username === username);
-  if (usernameExist) return "error";
-  return set("users", [...users, { username }]).then(() => "ok");
+  if (usernameExist) {
+    return "error";
+  }
+  await indexedDb.putValue(OBJECT_STORE_USERS, { username });
+  return "ok";
 };
 
 export const getUsers = () => {
-  return get("users").then((response) => response);
+  return indexedDb.getAllValue(OBJECT_STORE_USERS).then((response) => response);
 };
 
 export const deleteUser = async (username) => {
