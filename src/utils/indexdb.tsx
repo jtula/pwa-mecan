acef04134b3985a4af4bbdc402f6e590d2744bed
Jose A Tula Leyva <joseph.tula@gmail.com>
2021-02-22 22:10:13 -0500

    feat: add indexedDb class

---
+++ src/utils/indexdb.tsx
@@ -0,0 +1,82 @@
+//credits to: https://js.plainenglish.io/working-with-indexeddb-in-typescript-react-ad504a1bdae3
+
+import {DATABASE} from "../constants";
+
+import { IDBPDatabase, openDB } from 'idb';
+
+class IndexedDb {
+    private database: string;
+    private db: any;
+
+    constructor(database: string) {
+        this.database = database;
+    }
+
+    public async createObjectStore(tableNames: string[]) {
+        try {
+            this.db = await openDB(this.database, 1, {
+                upgrade(db: IDBPDatabase) {
+                    for (const tableName of tableNames) {
+                        if (db.objectStoreNames.contains(tableName)) {
+                            continue;
+                        }
+                        db.createObjectStore(tableName, { autoIncrement: true, keyPath: 'id' });
+                    }
+                },
+            });
+        } catch (error) {
+            return false;
+        }
+    }
+
+    public async getValue(tableName: string, id: number) {
+        const tx = this.db.transaction(tableName, 'readonly');
+        const store = tx.objectStore(tableName);
+        const result = await store.get(id);
+        console.log('Get Data ', JSON.stringify(result));
+        return result;
+    }
+
+    public async getAllValue(tableName: string) {
+        const tx = this.db.transaction(tableName, 'readonly');
+        const store = tx.objectStore(tableName);
+        const result = await store.getAll();
+        console.log('Get All Data', JSON.stringify(result));
+        return result;
+    }
+
+    public async putValue(tableName: string, value: object) {
+        const tx = this.db.transaction(tableName, 'readwrite');
+        const store = tx.objectStore(tableName);
+        const result = await store.put(value);
+        console.log('Put Data ', JSON.stringify(result));
+        return result;
+    }
+
+    public async putBulkValue(tableName: string, values: object[]) {
+        const tx = this.db.transaction(tableName, 'readwrite');
+        const store = tx.objectStore(tableName);
+        for (const value of values) {
+            const result = await store.put(value);
+            console.log('Put Bulk Data ', JSON.stringify(result));
+        }
+        return this.getAllValue(tableName);
+    }
+
+    public async deleteValue(tableName: string, id: number) {
+        const tx = this.db.transaction(tableName, 'readwrite');
+        const store = tx.objectStore(tableName);
+        const result = await store.get(id);
+        if (!result) {
+            console.log('Id not found', id);
+            return result;
+        }
+        await store.delete(id);
+        console.log('Deleted Data', id);
+        return id;
+    }
+}
+
+const indexedDb = new IndexedDb(DATABASE)
+
+export default indexedDb;
