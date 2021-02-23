2e0aba189cad1471b32d3083792608c6528a048d
Jose A Tula Leyva <joseph.tula@gmail.com>
2021-02-22 22:08:17 -0500

    feat: add constans

---
+++ src/constants/index.js
@@ -0,0 +1,4 @@
+export const DATABASE = "bussines";
+export const OBJECT_STORE_INCOMES = "incomes";
+export const OBJECT_STORE_EXPENSES = "expenses";
+export const OBJECT_STORE_PRINTERS = "printers";
