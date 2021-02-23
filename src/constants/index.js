e3b1fbd30450914d11ae54cfb145ed1492172357
Jose A Tula Leyva <joseph.tula@gmail.com>
2021-02-22 23:23:54 -0500

    feat: add user constant

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,5 @@
 export const DATABASE = "bussines";
+export const OBJECT_STORE_USERS = "users";
 export const OBJECT_STORE_INCOMES = "incomes";
 export const OBJECT_STORE_EXPENSES = "expenses";
 export const OBJECT_STORE_PRINTERS = "printers";
