7654546b658b3d3b1e572163201b2fc67657f53d
Jose A Tula Leyva <joseph.tula@gmail.com>
2021-02-23 22:32:46 -0500

    feat: handle onclick

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -1,7 +1,7 @@
 import React from "react";
 import TrashIcon from "src/components/svg/TrashIcon";
 
-const UserList = ({ users, handleDeleteUser }) => {
+const UserList = ({ users, handleLogin, handleDeleteUser }) => {
   if (!users.length)
     return <span className="p-2">No se encontraron usuarios</span>;
 
@@ -12,7 +12,9 @@ const UserList = ({ users, handleDeleteUser }) => {
           className="flex p-3 rounded-md hover:bg-gray-200 items-center "
           key={idx}
         >
-          <span className="flex-1">{user.username}</span>
+          <span onClick={() => handleLogin(user.id)} className="flex-1">
+            {user.username}
+          </span>
           <button
             onClick={() => handleDeleteUser(user.id)}
             className="inline-flex items-center justify-center w-15 h-15 rounded-full"
