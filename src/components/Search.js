d580d833a8c7fc6d11377c41d562c091a6b50464
Jose A Tula Leyva <joseph.tula@gmail.com>
2021-02-25 17:05:44 -0500

    feat: add search component

---
+++ src/components/Search.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Search = () => {
+  return (
+    <div className="flex shadow-sm rounded">
+      <span className="flex justify-end items-center text-gray-500 p-2">
+        <img width="20" height="20" alt="search" src="icons/search.svg" />
+      </span>
+      <input
+        type="text"
+        className="bg-purple-white border-0 p-3 focus:outline-none focus:ring-0"
+        placeholder="Buscar..."
+      />
+    </div>
+  );
+};
+
+export default Search;
--- src/layouts/MainLayout.js
+++ src/layouts/MainLayout.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import Profile from "src/components/Profile";
+import Search from "src/components/Search";
 import NavBar from "./NavBar";
 
 export default function AppLayout({ children }) {
@@ -10,6 +11,8 @@ export default function AppLayout({ children }) {
       <div>
         <NavBar />
         <header className="flex align-items mx-12 p-4">
+          <Search />
+          <div className="flex-grow"></div>
           <Profile />
         </header>
         <main className="container-lg mx-12 p-4">{children}</main>
