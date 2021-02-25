67777c7c3e765dd3ec09a4e83759a10acbd7b5f1
Jose A Tula Leyva <joseph.tula@gmail.com>
2021-02-25 11:49:23 -0500

    fix: change class

--- src/layouts/NavBar.js
+++ src/layouts/NavBar.js
@@ -2,16 +2,16 @@ import React from "react";
 
 const NavBar = () => {
   return (
-    <nav className="flex flex-col w-12 pt-5 shadow-md h-screen">
+    <nav className="flex flex-col w-12 pt-5 shadow-md h-screen absolute">
       <div className="flex items-center justify-center">
         <img className="h-8 w-8" src="money-bag192.png" alt="Workflow" />
       </div>
       <div className="flex flex-grow py-10">
         <div>
-          <button class="inline-flex items-center justify-center w-10 h-10 ml-1 transition-colors duration-150 rounded-full focus:shadow-outline hover:bg-gray-100">
+          <button className="inline-flex items-center justify-center w-10 h-10 ml-1 transition-colors duration-150 rounded-full focus:shadow-outline hover:bg-gray-100">
             <img width="20" height="20" src="icons/grid.svg" alt="dashboard" />
           </button>
-          <button class="inline-flex items-center justify-center w-10 h-10 ml-1 transition-colors duration-150 rounded-full focus:shadow-outline hover:bg-gray-100">
+          <button className="inline-flex items-center justify-center w-10 h-10 ml-1 transition-colors duration-150 rounded-full focus:shadow-outline hover:bg-gray-100">
             <img
               width="20"
               height="20"
@@ -22,7 +22,7 @@ const NavBar = () => {
         </div>
       </div>
       <div className="pb-5 -inset-y-0">
-        <button class="inset-y-0 inline-flex items-center justify-center w-10 h-10 ml-1 transition-colors duration-150 rounded-full focus:shadow-outline hover:bg-gray-100">
+        <button className="inset-y-0 inline-flex items-center justify-center w-10 h-10 ml-1 transition-colors duration-150 rounded-full focus:shadow-outline hover:bg-gray-100">
           <img width="20" height="20" src="icons/cog.svg" alt="configuration" />
         </button>
       </div>
