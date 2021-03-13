99a1d14d3608467c379638fb270a59efe7ef7072
Jose A Tula Leyva <joseph.tula@gmail.com>
2021-03-13 18:45:01 -0500

    fix: check userlogged

--- src/pages/Incomes.js
+++ src/pages/Incomes.js
@@ -1,8 +1,18 @@
 import React from "react";
+import useUser from "src/hooks/useUser";
 import MainLayout from "src/layouts/MainLayout";
 import Calendar from "src/components/Calendar";
+import { useLocation } from "wouter";
 
 const Incomes = () => {
+  const { user } = useUser();
+  const [_, pushLocation] = useLocation();
+
+  if (!user) {
+    pushLocation("/");
+    return null;
+  }
+
   return (
     <MainLayout>
       <h2 className="px-6">Ingresos</h2>
