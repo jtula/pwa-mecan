ea41dfd1ce47ae9d1efc6918e534271a13fe6216
Jose A Tula Leyva <joseph.tula@gmail.com>
2021-06-06 18:40:40 -0500

    feat: add getIncomesByUser service

--- src/services/incomes.js
+++ src/services/incomes.js
@@ -3,19 +3,26 @@ import { OBJECT_STORE_INCOMES } from "src/constants";
 
 const tableName = OBJECT_STORE_INCOMES.name;
 
+export const getIncomesByUser = async (user) => {
+  const result = await indexedDb.getAllValuesFromIndex(
+    tableName,
+    "by_user",
+    user
+  );
+  return result;
+};
+
 export const add = async ({ income, user, createdAt }) => {
   const newIncome = { income, user, createdAt };
-  const result = await indexedDb.getValueFromIndex(
-    tableName,
-    "by_createdAt",
-    createdAt
-  );
+  const result = await indexedDb.getValueFromIndex(tableName, "by_createdAt", [
+    createdAt,
+    user,
+  ]);
 
   if (result) {
     newIncome.income += result.income;
   }
 
   await indexedDb.putValue(tableName, newIncome);
-
   return "ok";
 };
