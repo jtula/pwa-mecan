8f84efb40c21a9a042271ee40a82fc8d0c33d3fa
Jose A Tula Leyva <joseph.tula@gmail.com>
2021-06-06 18:42:12 -0500

    fix: handle new incomes

--- src/components/NewIncomeHandler.js
+++ src/components/NewIncomeHandler.js
@@ -2,14 +2,16 @@ import NewIncomeForm from "./NewIncomeForm";
 import { add } from "src/services/incomes";
 import useUser from "src/hooks/useUser";
 
-const NewIncomeHandler = ({ open, setOpen, createdAt }) => {
+const NewIncomeHandler = ({ open, setOpen, createdAt, handleNewIncomes }) => {
   const { user } = useUser();
 
   const handleSubmit = (value) => {
     const income = parseFloat(value);
-    add({ income, user: user.username, createdAt }).then((res) =>
-      console.log(res)
-    );
+    add({ income, user: user.username, createdAt }).then((res) => {
+      if (res === "ok") {
+        handleNewIncomes();
+      }
+    });
   };
 
   return (
