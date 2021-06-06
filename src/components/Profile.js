b93937e33be52cb8f8a85779d3b338eaa15249f7
Jose A Tula Leyva <joseph.tula@gmail.com>
2021-06-06 09:34:35 -0500

    fix: remove transitions props

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,10 +1,11 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import { Transition } from "@headlessui/react";
 import useUser from "src/hooks/useUser";
 
 const Profile = () => {
   const { user, logout } = useUser();
   const [userMenuOpen, setUserMenuOpen] = useState(false);
+  const perfilButtonRef = useRef(null);
 
   return (
     <>
@@ -15,7 +16,7 @@ const Profile = () => {
             className="max-w-xs rounded-full flex items-center text-sm focus:outline-none space-x-2"
             id="user-menu"
             aria-haspopup="true"
-            onClick={() => setUserMenuOpen(!userMenuOpen)}
+            onClick={() => setUserMenuOpen((userMenuOpen) => !userMenuOpen)}
           >
             <span className="sr-only">Open user menu</span>
             <img
@@ -42,38 +43,28 @@ const Profile = () => {
             </svg>
           </button>
         </div>
-        <Transition
-          show={userMenuOpen}
-          enter="transition ease-out duration-100"
-          enterFrom="transform opacity-0 scale-95"
-          enterTo="transform opacity-100 scale-100"
-          leave="transition ease-in duration-75"
-          leaveFrom="transform opacity-100 scale-100"
-          leaveTo="transform opacity-0 scale-95"
-        >
-          {(ref) => (
-            <div
-              ref={ref}
-              className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5"
-              role="menu"
-              aria-orientation="vertical"
-              aria-labelledby="user-menu"
+        <Transition show={userMenuOpen}>
+          <div
+            ref={perfilButtonRef}
+            className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5"
+            role="menu"
+            aria-orientation="vertical"
+            aria-labelledby="user-menu"
+          >
+            <a
+              href="/profile"
+              className="font-medium uppercase block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+              role="menuitem"
             >
-              <a
-                href="/profile"
-                className="font-medium uppercase block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-                role="menuitem"
-              >
-                Perfil
-              </a>
-              <button
-                className="font-medium text-left w-full uppercase block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 focus:outline-none focus:ring-0"
-                onClick={logout}
-              >
-                Salir
-              </button>
-            </div>
-          )}
+              Perfil
+            </a>
+            <button
+              className="font-medium text-left w-full uppercase block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 focus:outline-none focus:ring-0"
+              onClick={logout}
+            >
+              Salir
+            </button>
+          </div>
         </Transition>
       </div>
     </>
