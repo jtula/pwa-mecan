e7163065e91955162ed249445bc2b7c2eb9b975b
Jose A Tula Leyva <joseph.tula@gmail.com>
2021-06-10 15:18:46 -0500

    fix: change color

--- src/components/NewExpenseForm.js
+++ src/components/NewExpenseForm.js
@@ -66,9 +66,9 @@ const NewExpenseForm = ({
               >
                 <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
                   <div className="sm:flex sm:items-start">
-                    <div className="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-green-100 sm:mx-0 sm:h-10 sm:w-10">
+                    <div className="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
                       <CurrencyDollarIcon
-                        className="h-6 w-6 text-green-600"
+                        className="h-6 w-6 text-red-600"
                         aria-hidden="true"
                       />
                     </div>
@@ -90,7 +90,7 @@ const NewExpenseForm = ({
                           value={expense}
                           onChange={handleExpense}
                           type="number"
-                          className="mx-auto appearance-none rounded relative block w-24 my-3 px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-green-500 focus:z-10 sm:text-sm"
+                          className="mx-auto appearance-none rounded relative block w-24 my-3 px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-red-500 focus:z-10 sm:text-sm"
                         />
                       </div>
                     </div>
@@ -99,7 +99,7 @@ const NewExpenseForm = ({
                 <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
                   <button
                     type="submit"
-                    className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-green-600 text-base font-medium text-white hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500 sm:ml-3 sm:w-auto sm:text-sm"
+                    className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm"
                     onClick={() => setOpen(false)}
                   >
                     Adicionar
