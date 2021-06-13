916701d9838ae8b8b72e36265c3003a467462d9a
Jose A Tula Leyva <joseph.tula@gmail.com>
2021-06-12 21:42:02 -0500

    fix: change onClick level

--- src/components/CalendarBody.js
+++ src/components/CalendarBody.js
@@ -22,12 +22,10 @@ const CalendarBody = ({
         {noOfDays.map((day, idx) => (
           <div
             key={idx}
-            className="w-1/7 lg:h-20 sm:h-28 px-2 pt-2 border-r border-b relative"
+            onClick={() => showNewItemModal(`${year}-${month + 1}-${day}`)}
+            className="w-1/7 lg:h-20 sm:h-28 px-2 pt-2 border-r border-b relative cursor-pointer"
           >
-            <div
-              onClick={() => showNewItemModal(`${year}-${month + 1}-${day}`)}
-              className="inline-flex w-6 h-6 items-center justify-center cursor-pointer text-center leading-none rounded-full transition ease-in-out duration-100"
-            >
+            <div className="inline-flex w-6 h-6 items-center justify-center text-center leading-none rounded-full transition ease-in-out duration-100">
               {day}
             </div>
             <div className="lg:flex overflow-y-visi e mt-1 relative">
@@ -40,10 +38,7 @@ const CalendarBody = ({
                 .map((itemFiltered, idx) => (
                   <div
                     key={idx}
-                    className={`w-full px-1 py-1 mr-1 rounded-lg cursor-pointer border bg-${theme.color}-200`}
-                    onClick={() =>
-                      showNewItemModal(`${year}-${month + 1}-${day}`)
-                    }
+                    className={`w-full px-1 py-1 mr-1 rounded-lg border bg-${theme.color}-200`}
                   >
                     <p className="text-sm text-center">{itemFiltered.value}</p>
                   </div>
