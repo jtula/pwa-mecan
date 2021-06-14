adbe2255b32a28a1c7a3b7908a5f6c14026c97bf
Jose A Tula Leyva <joseph.tula@gmail.com>
2021-06-13 21:46:53 -0500

    feat: fetching data

--- src/pages/Reports/Expenses.js
+++ src/pages/Reports/Expenses.js
@@ -1,12 +1,26 @@
+import React, { useEffect, useState } from "react";
+import useUser from "src/hooks/useUser";
 import DashboardCard from "src/components/DashboardCard";
 import ArrowDownIcon from "@heroicons/react/outline/ArrowDownIcon";
+import { getExpensesByUser } from "src/services/expenses";
 
 const Expenses = () => {
+  const user = useUser();
+  const [expenses, setExpenses] = useState(0);
+
+  useEffect(() => {
+    getExpensesByUser(user.username).then((response) => {
+      setExpenses(
+        response.reduce((acc, el) => ({ value: acc.value + el.value }))
+      );
+    });
+  }, [user]);
+
   return (
     <DashboardCard
       title={"Gastos"}
       icon={<ArrowDownIcon width="25" height="25" color="#ec407a" />}
-      value={"$1289.00"}
+      value={`$${expenses.value}`}
     />
   );
 };
--- src/pages/Reports/Incomes.js
+++ src/pages/Reports/Incomes.js
@@ -1,12 +1,26 @@
+import React, { useEffect, useState } from "react";
 import DashboardCard from "src/components/DashboardCard";
 import ArrowUpIcon from "@heroicons/react/outline/ArrowUpIcon";
+import useUser from "src/hooks/useUser";
+import { getIncomesByUser } from "src/services/incomes";
 
 const Incomes = () => {
+  const user = useUser();
+  const [incomes, setIncomes] = useState(0);
+
+  useEffect(() => {
+    getIncomesByUser(user.username).then((response) => {
+      setIncomes(
+        response.reduce((acc, el) => ({ value: acc.value + el.value }))
+      );
+    });
+  }, [user]);
+
   return (
     <DashboardCard
       title={"Ingresos"}
       icon={<ArrowUpIcon width="25" height="25" color="#42a5f5" />}
-      value={"$1289.00"}
+      value={`$${incomes.value}`}
     />
   );
 };
